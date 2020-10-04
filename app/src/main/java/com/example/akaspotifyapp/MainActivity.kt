1f2b3601eb325bba933873ce8838dfc07281e160
amiratashani <amiratashanii@gmail.com>
2020-10-04 16:18:14 +0330

    add hilt in project and test it add ui for MainActivity

--- app/src/main/java/com/example/akaspotifyapp/MainActivity.kt
+++ app/src/main/java/com/example/akaspotifyapp/MainActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.RequestManager
 import com.example.akaspotifyapp.data.remote.MusicDatabase
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             val songs = musicDatabase.getAllSongs()
             Log.i("MainActivityCustome", songs.toString())
         }
+
 
 
     }
