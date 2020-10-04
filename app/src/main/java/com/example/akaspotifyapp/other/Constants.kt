5dea9d6eca15144b85d86febfbe4536029c0d1ad
amiratashani <amiratashanii@gmail.com>
2020-10-04 22:20:59 +0330

    end MusicNotificationManager and MusicPlayerNotificationListener

--- app/src/main/java/com/example/akaspotifyapp/other/Constants.kt
+++ app/src/main/java/com/example/akaspotifyapp/other/Constants.kt
@@ -3,4 +3,6 @@ package com.example.akaspotifyapp.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
